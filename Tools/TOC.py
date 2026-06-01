b64ee8e4e11e40de0319083bcc9d5e161658b5c1
Barnold88 <bandslover@outlook.com>
2026-06-01 14:30:28 +0100

    Add buggy formatting function

--- Tools/TOC.py
+++ Tools/TOC.py
@@ -1,20 +1,43 @@
 # Just a helper file to auto generate table of contents for myslef in my MD files
 import re
+import string
+from typing import List
 
-def generateTOC(path: str)->None:
+# List[str]
+
+def format_heading(heading:str)-> str:
+
+    print(f"unformatted heading {heading}")
+
+    allowed_chars = string.ascii_lowercase + '#' + ' '
+    print(f"Allowed chars {allowed_chars}")
+
+    heading = ''.join(e for e in heading if e in allowed_chars) # May not be correct for heading formats in general but its a tool for me
+    heading = re.sub(r'\s+', '-', heading)
+
+    return heading
+
+def grab_headings(lines: List[str]) -> List[str]:
 
     heading_regex = r'^(#+)\s*(.+?)(\s*\(.*?\))?$'
-
     headings = []
+    for line in lines:
+        if re.match(heading_regex, line):
+            heading = format_heading(line)
+            headings.append(heading)
+    return headings
+
+
+def generate_TOC(path: str)->None:
 
     try:
 
         with open(path,mode="r",encoding="utf-8") as file:
+            headings = grab_headings(file.readlines())
 
-            lines = file.readlines()
-            for line in lines:
-                if re.match(heading_regex, line):
-                    print(line)
+            for heading in headings:
+                print(heading)
+
 
     except FileNotFoundError as FE:
         print(f"Error detected, possible not file found error. Ensure that the path is correct. The path needs to be either absolute or relative from the current execution location.\n\nERROR: {FE}")
@@ -25,4 +48,4 @@ if __name__ == "__main__":
 
     markdown_path = input("Please enter the path of the MD file you wish to generate a table of contents for: ")
 
-    generateTOC(markdown_path)
+    headings = generate_TOC(markdown_path)
