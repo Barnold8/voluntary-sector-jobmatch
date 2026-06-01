0557310781c4e74f9a49a2873a58673c8032a63b
Barnold88 <bandslover@outlook.com>
2026-06-01 14:10:15 +0100

    Add error handling on file reading

--- Tools/TOC.py
+++ Tools/TOC.py
@@ -7,14 +7,18 @@ def generateTOC(path: str)->None:
 
     headings = []
 
+    try:
 
-    with open(path,mode="r",encoding="utf-8") as file:
+        with open(path,mode="r",encoding="utf-8") as file:
 
-        lines = file.readlines()
-        for line in lines:
-            print(line)
-            if re.match(heading_regex, line):
-                print(line)
+            lines = file.readlines()
+            for line in lines:
+                if re.match(heading_regex, line):
+                    print(line)
+
+    except FileNotFoundError as FE:
+        print(f"Error detected, possible not file found error. Ensure that the path is correct. The path needs to be either absolute or relative from the current execution location.\n\nERROR: {FE}")
+        exit(1)
 
 
 if __name__ == "__main__":
